6a7623f88679995e04a5d4d5e86e1b04e22d7958
ocamarli <108758911+ocamarli@users.noreply.github.com>
2023-10-24 08:47:15 -0600

    actualizacion app y funciones

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask
 from flask import jsonify
+from requests import HTTPError
 from config import config
 from flask_cors import CORS
 import pandas as pd
@@ -8,41 +9,54 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from funciones import *
+import os
+
+import csv
+if not os.path.exists('data.csv'):
+    # Si el archivo no existe, crea uno utilizando 
+    df = cargar_datos('https://raw.githubusercontent.com/jdanieln/UNIR_Investigacion_IA_Backend/main/data/data.csv')    
+
+    selected_columns = ["Product", "Quantity Ordered", "Order Date"]
+    df = seleccionar_columnas(df,selected_columns)
+    df = manejo_valores_faltantes(df)
+    df = tratamiento_columnas(df)    
+    try:
+    # Convierte el DataFrame en un archivo CSV
+        df.to_csv('data.csv', index=False)
+        print(f"Archivo CSV  creado en la misma carpeta del proyecto.")
+    except Exception as e:
+        print(f"Error al crear el archivo CSV: {e}")
+
+try:
+    # Intenta leer el archivo CSV como un DataFrame
+    df = pd.read_csv('data.csv')
+    print("El archivo CSV ok")
+except FileNotFoundError:
+    # Si el archivo no se encuentra, realiza otra acción (por ejemplo, muestra un mensaje)
+
+    print("El archivo CSV no se encontró en la misma carpeta del proyecto.")
+ 
 
 def create_app(env):
     app = Flask(__name__)
     CORS(app)
     app.config['DEBUG'] = env.DEBUG
     app.config.from_object(env)
-
+    app.config['df'] = df
     return app
 
 env = config['development']
 app = create_app(env)
-def convert_date(date_str):
-    try:
-        return pd.to_datetime(date_str, format='%m/%d/%Y %H:%M')
-    except ValueError:
-        return pd.to_datetime(date_str, format='%m/%d/%y %H:%M')
-
-@app.route('/upload', methods=['POST'])
-def upload_csv():
-    global df
-    df = cargar_datos('https://raw.githubusercontent.com/jdanieln/UNIR_Investigacion_IA_Backend/main/data/data.csv')    
-    return 'CSV en la solicitud.', 200
 
 flag_csv=False
 @app.route("/dataframe")
 def dataFrame():
-    df = cargar_datos('https://raw.githubusercontent.com/jdanieln/UNIR_Investigacion_IA_Backend/main/data/data.csv')    
-    selected_columns = ["Product", "Quantity Ordered", "Order Date"]
-    df = seleccionar_columnas(df,selected_columns)
-    df = manejo_valores_faltantes(df)
-    dfToFront=df.copy()
-    df = tratamiento_columnas(df)
 
+    df = app.config['df'] 
+    print(df.head(10))
+    df['Order Date'] = df['Order Date'].apply(convert_date_str)
     # Convertir el DataFrame a JSON
-    df_json = dfToFront.to_json(orient='records')
+    df_json = df.to_json(orient='records')
     resumen_top_10_productos_mas_vendidos=top_10_productos_mas_vendidos(df,False,True)
     resumen_frecuencia_de_ventas_por_producto=frecuencia_de_ventas_por_producto(df,False,True)
     resumen_distribucion_de_la_cantidad_vendida_de_productos=distribucion_de_la_cantidad_vendida_de_productos(df,False,True)
@@ -63,14 +77,65 @@ def dataFrame():
     }
     resultados_json = json.dumps(resultados_dict)
     return jsonify({"dataframe":df_json,"resumenes":resultados_json})
-
-@app.route("/home")
+def root():
+    return "Works!!"
+@app.route("/test")
+def test():
+    df = app.config['df']  # Obtener el DataFrame desde la configuración de la aplicación
+    # Imprimir los primeros 10 registros del DataFrame
+    first_10_records = df.head(10)
+    return str(first_10_records)
+@app.route("/createCSV")
+def createCSV(dataframe, csv_filename):
+    try:
+        # Convierte el DataFrame en un archivo CSV
+        dataframe.to_csv(csv_filename, index=False)
+        print(f"Archivo CSV '{csv_filename}' creado en la misma carpeta del proyecto.")
+        return "OK", 200
+    except Exception as e:
+        print(f"Error al crear el archivo CSV: {e}")
+        return "Error al crear el archivo CSV", 500
 def root():
     return "Works!!"
 @app.route("/getPrediction/<string:start_date>/<string:end_date>/<string:product>", methods=["GET"])
 def get_sales_by_date(start_date, end_date,product):
-    results=""
-    return jsonify(status=True, data=results), 200
+    try:
+        # Cargar el modelo previamente entrenado
+        loaded_model = load_model("./../modelo-ia/modelo.keras")
+
+        # Preprocesar las fechas y otros datos según sea necesario
+        # Aquí asumimos que las fechas se proporcionan en formato "YYYY-MM-DD HH:mm:ss"
+        start_date = datetime.strptime(start_date, "%Y-%m-%d %H:%M:%S")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d %H:%M:%S")
+
+        # Aquí puedes realizar cualquier otro preprocesamiento necesario de los datos de entrada
+        # Por ejemplo, codificar la variable categórica "product" si es necesario
+
+        # Crear un ejemplo de input_data (asegúrate de que tenga las mismas características que se utilizaron durante el entrenamiento)
+        # En este ejemplo, se asume que tienes un conjunto de características que coincide con las que se utilizaron en el modelo
+        input_data = np.array([[
+        start_date.year, start_date.month, start_date.day, start_date.hour, start_date.minute, start_date.weekday(),
+        product  # Añade la variable "product" codificada aquí si es necesario
+        ]])
+
+        # Realizar la predicción
+        predictions = loaded_model.predict(input_data)
+
+        # Las predicciones se pueden ajustar según sea necesario antes de incluirlas en la respuesta JSON
+
+        # Convertir las predicciones a una lista de Python
+        predictions_list = predictions.tolist()
+
+        # Crear un diccionario que contenga las predicciones
+        response = {
+        "predictions": predictions_list
+        }
+
+        return jsonify(status=True, data=response), 200
+
+    except Exception as e:
+        return jsonify(status=False, error=str(e)), 500
+
 if __name__ == '__main__':
     app.run()
  
--- api/funciones.py
+++ api/funciones.py
@@ -8,189 +8,285 @@ import matplotlib.pyplot as plt
 
 
 
+# Función para manejar valores faltantes en un DataFrame
 def manejo_valores_faltantes(df):
+    # Calcula la cantidad de valores nulos en cada columna
     faltantes = df.isnull().sum()
     print(faltantes)
+    
+    # Elimina filas con todos los valores nulos
     df.dropna(how='all')
+    
+    # Convierte la columna 'Quantity Ordered' a valores numéricos y elimina filas con valores nulos
     df['Quantity Ordered'] = pd.to_numeric(df['Quantity Ordered'], errors='coerce')
     df = df.dropna(how='all')
+    
+    # Muestra información sobre el DataFrame
     df.info()
+    
+    # Calcula el número de filas eliminadas
     num_rows_before = df.shape[0]
     df.dropna(inplace=True)
     num_rows_after = df.shape[0]
     num_rows_deleted = num_rows_before - num_rows_after
-    print("Número de filas eliminadas:", num_rows_deleted)    
+    print("Número de filas eliminadas:", num_rows_deleted)
+    
     return df
-
+# Función para seleccionar columnas específicas de un DataFrame
 def seleccionar_columnas(df, columnas):
     return df[columnas]
 
+# Función para convertir una cadena de fecha en un objeto de fecha y hora
 def convert_date(date_str):
     try:
-        return pd.to_datetime(date_str, format='%m/%d/%Y %H:%M')
-    except ValueError:
+        # Intenta convertir la fecha con formato 'MM/DD/YYYY HH:MM'
         return pd.to_datetime(date_str, format='%m/%d/%y %H:%M')
-
+        
+    except ValueError:
+        # Si falla, intenta con formato 'MM/DD/YY HH:MM'
+          return pd.to_datetime(date_str, format='%m/%d/%Y %H:%M')
+def convert_date_str(date_str):
+    try:
+        # Intenta convertir la fecha con formato 'MM/DD/YYYY HH:MM'
+        return pd.to_datetime(date_str, format='%Y-%m-%d %H:%M:%S')
+        
+    except ValueError:
+        # Si falla, intenta con formato 'MM/DD/YY HH:MM'
+          return pd.to_datetime(date_str, format='%Y-%m-%d %H:%M:%S')
+# Función para cargar datos desde un archivo CSV
 def cargar_datos(nombre_archivo):
-    
     df = pd.read_csv(nombre_archivo)
-    df.info()    
+    df.info()
     return df
 
+# Función para realizar el tratamiento de columnas relacionadas con fechas
 def tratamiento_columnas(df):
+    
+    
+    df['Order Date'] = df['Order Date'].apply(convert_date)
+    df['Year'] = df['Order Date'].dt.year
+    df['Month'] = df['Order Date'].dt.month
+    df['Day'] = df['Order Date'].dt.day
+    df['Hour'] = df['Order Date'].dt.hour
+    df['Minute'] = df['Order Date'].dt.minute
+    df['Day of Week'] = df['Order Date'].dt.dayofweek
+    # Muestra información sobre el DataFrame después del tratamiento
+    df.info()
+    
+    return df
 
-  df['Order Date'] = df['Order Date'].apply(convert_date)
-  df['Year'] = df['Order Date'].dt.year
-  df['Month'] = df['Order Date'].dt.month
-  df['Day'] = df['Order Date'].dt.day
-  df['Hour'] = df['Order Date'].dt.hour
-  df['Minute'] = df['Order Date'].dt.minute
-  print("ULTIMO")
-  df.info()
-  return df
-
+# Función para manejar registros duplicados en un DataFrame
 def manejo_duplicados(df):
+    # Busca y elimina registros duplicados
     duplicados = df.duplicated()
     df.drop_duplicates(inplace=True)
+    
     return df
 
+
+# Función para obtener estadísticas descriptivas de un DataFrame
 def obtener_datos_descriptivos(df):
-  print(df.describe())
-  print(df.describe(include=['O']))
-  unique_products = df['Product'].unique()
-  print(unique_products)
-  media_cantidad = df['Quantity Ordered'].mean()
-  mediana_cantidad = df['Quantity Ordered'].median()
-  desviacion_estandar_cantidad = df['Quantity Ordered'].std()
-  resumen_cantidad = df['Quantity Ordered'].describe()
+    # Muestra estadísticas descriptivas para columnas numéricas
+    print(df.describe())
+    
+    # Muestra estadísticas descriptivas para columnas categóricas
+    print(df.describe(include=['O']))
+    
+    # Obtiene los productos únicos en la columna 'Product'
+    unique_products = df['Product'].unique()
+    print(unique_products)
+    
+    # Calcula la media, mediana y desviación estándar de la cantidad vendida
+    media_cantidad = df['Quantity Ordered'].mean()
+    mediana_cantidad = df['Quantity Ordered'].median()
+    desviacion_estandar_cantidad = df['Quantity Ordered'].std()
+    resumen_cantidad = df['Quantity Ordered'].describe()
 
-def top_10_productos_mas_vendidos(df,mostrar_grafico,mostrar_resumen):
-  top_products = df.groupby('Product')['Quantity Ordered'].sum().nlargest(10)
+# Función para identificar y visualizar los 10 productos más vendidos
+def top_10_productos_mas_vendidos(df, mostrar_grafico, mostrar_resumen):
+    # Agrupa los productos y suma las cantidades vendidas, luego selecciona los 10 productos con mayores ventas
+    top_products = df.groupby('Product')['Quantity Ordered'].sum().nlargest(10)
 
-  # Obtén el producto más vendido
-  producto_mas_vendido = top_products.idxmax()
-  cantidad_mas_vendida = top_products.max()
-  resumen = "El gráfico muestra los 10 productos más vendidos.\n"   
-  resumen=f"El producto más vendido fue '{producto_mas_vendido}' con una cantidad de {cantidad_mas_vendida} unidades vendidas."
- 
-  
-  if mostrar_grafico:
-    sns.barplot(x=top_products.index, y=top_products.values)
-    plt.xticks(rotation=90)
-    plt.xlabel('Producto')
-    plt.ylabel('Cantidad Vendida')
-    plt.title('Productos Más Vendidos por Cantidad Vendida')
-  if mostrar_resumen:
-    print(resumen)
-  return resumen 
+    # Obtiene el producto más vendido y la cantidad más vendida
+    producto_mas_vendido = top_products.idxmax()
+    cantidad_mas_vendida = top_products.max()
 
-def frecuencia_de_ventas_por_producto(df,mostrar_grafico,mostrar_resumen):
-  product_frequency = df['Product'].value_counts()
-  plt.figure(figsize=(12, 6))
-  product_frequency.plot(kind='bar', color='blue')
-  producto_mas_frecuente = product_frequency.idxmax()
-  frecuencia_maxima = product_frequency.max()
-  resumen = "El gráfico muestra la frecuencia de ventas de los productos:.\\n"
-  resumen += f"El producto más frecuente es '{producto_mas_frecuente}' con una frecuencia de {frecuencia_maxima} compras."
-  if mostrar_grafico:
-    plt.xlabel('Producto')
-    plt.ylabel('Frecuencia')
-    plt.title('Análisis de Frecuencia de Productos')
-    plt.xticks(rotation=90)
-    plt.grid(axis='y')    
-    plt.show()
-  if mostrar_resumen:
-    print(resumen)
-  return resumen
+    # Crea un resumen
+    resumen = "El gráfico muestra los 10 productos más vendidos.\n"
+    resumen += f"El producto más vendido fue '{producto_mas_vendido}' con una cantidad de {cantidad_mas_vendida} unidades vendidas."
 
-def distribucion_de_la_cantidad_vendida_de_productos(df, mostrar_grafico, mostrar_resumen):
-    # Calcular el valor más frecuente (moda)
-    moda = df['Quantity Ordered'].mode().values[0] 
-    resumen = "El gráfico la distribución de la cantidad de productos vendidos en función de la frecuencia de ventas.\n"
-    resumen += "La cantidad más frecuente vendida es"+ str(moda)+ "unidades."     
     if mostrar_grafico:
-      plt.hist(df['Quantity Ordered'], bins=20, edgecolor='k')
-      plt.xlabel('Cantidad Vendida')
-      plt.ylabel('Frecuencia')
-      plt.title('Distribución de Cantidad Vendida')        
-      plt.show()
+        # Muestra un gráfico de barras de los productos más vendidos
+        sns.barplot(x=top_products.index, y=top_products.values)
+        plt.xticks(rotation=90)
+        plt.xlabel('Producto')
+        plt.ylabel('Cantidad Vendida')
+        plt.title('Productos Más Vendidos por Cantidad Vendida')
 
     if mostrar_resumen:
-      print(resumen)
-    return resumen 
-def ventas_por_mes_y_dia_de_la_semana(df,mostrar_grafico,mostrar_resumen):
-  df['Month'] = df['Order Date'].dt.month
-  df['Day of Week'] = df['Order Date'].dt.dayofweek
-  heatmap_data = df.pivot_table(index='Month', columns='Day of Week', values='Quantity Ordered', aggfunc='sum')
-  mes_max_ventas = heatmap_data.sum(axis=1).idxmax()
-  dia_max_ventas = heatmap_data.sum().idxmax()
-  max_ventas = heatmap_data.loc[mes_max_ventas, dia_max_ventas]
-  mes_min_ventas = heatmap_data.sum(axis=1).idxmin()
-  dia_min_ventas = heatmap_data.sum().idxmin()
-  min_ventas = heatmap_data.loc[mes_min_ventas, dia_min_ventas]
-  total_ventas = heatmap_data.sum().sum()
-  promedio_ventas_diario = heatmap_data.mean().mean()
-  resumen = "En el análisis del mapa de calor de ventas por mes y día de la semana, se encontraron las siguientes estadísticas:\n"
-  resumen += "El mes con la mayor cantidad de ventas es " + str(mes_max_ventas) + ", con un total de " + str(max_ventas) + " ventas.\n"
-  resumen += "El día de la semana con la mayor cantidad de ventas es " + str(dia_max_ventas) + ".\n"
-  resumen += "El día de la semana con la menor cantidad de ventas es " + str(dia_min_ventas) + "."
-  resumen += "El total de ventas en el conjunto de datos es de " + str(total_ventas) + " unidades."
-  resumen += "El promedio de ventas por día de la semana es de aproximadamente " + "{:.2f}".format(promedio_ventas_diario) + " unidades."
+        print(resumen)
 
-  if mostrar_grafico:
-    plt.figure(figsize=(12, 6))
-    sns.heatmap(heatmap_data, cmap='YlGnBu', linewidths=.5)
-    plt.title('Mapa de Calor de Ventas por Mes y Día de la Semana')
-    plt.xlabel('Día de la Semana')
-    plt.ylabel('Mes')
-    plt.xticks(ticks=[0, 1, 2, 3, 4, 5, 6], labels=['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'])
-    plt.yticks(rotation=0)
-    plt.show()      
-  if mostrar_resumen:
-    print(resumen)
-  return resumen 
-def ventas_por_hora_del_dia_y_de_la_semana(df,mostrar_grafico,mostrar_resumen):
-  df['Hour'] = df['Order Date'].dt.hour
-  df['Day of Week'] = df['Order Date'].dt.dayofweek 
+    return resumen
 
-  heatmap_data =df.pivot_table(index='Hour', columns='Day of Week', values='Quantity Ordered', aggfunc='sum')
-  dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+# Función para analizar la frecuencia de ventas por producto
+def frecuencia_de_ventas_por_producto(df, mostrar_grafico, mostrar_resumen):
+    # Calcula la frecuencia de ventas de cada producto
+    product_frequency = df['Product'].value_counts()
 
-  hora_pico_total = heatmap_data.idxmax().values
-  dia_pico_total = heatmap_data.idxmax(axis=1).idxmax()  
-  resumen = "Resumen del Mapa de Calor de Cantidad Vendida por Hora del Día y Día de la Semana:"
-  resumen += "El mapa de calor muestra las horas del día y los días de la semana con las mayores ventas."
-  resumen += "En general, la hora de mayor cantidad vendida es a las" + str(hora_pico_total)+", y el día de la semana con la mayor cantidad vendida es el día "+ str(dia_pico_total) +"."
+    if mostrar_grafico:
+        # Muestra un gráfico de barras de la frecuencia de ventas
+        plt.figure(figsize=(12, 6))
+        product_frequency.plot(kind='bar', color='blue')
+        plt.xlabel('Producto')
+        plt.ylabel('Frecuencia')
+        plt.title('Análisis de Frecuencia de Productos')
+        plt.xticks(rotation=90)
+        plt.grid(axis='y')
+        plt.show()
 
-  if mostrar_grafico:
-    plt.figure(figsize=(12, 6))
-    sns.heatmap(heatmap_data, cmap='YlGnBu', linewidths=.5)
-    plt.title('Mapa de Calor de Cantidad Vendida por Hora del Día y Día de la Semana')
-    plt.xlabel('Día de la Semana')
-    plt.ylabel('Hora del Día')  
-    plt.grid(True)
-    plt.show()
-  if mostrar_resumen:
-    print(resumen)
-  return resumen 
+    # Obtiene el producto más frecuente y su frecuencia máxima
+    producto_mas_frecuente = product_frequency.idxmax()
+    frecuencia_maxima = product_frequency.max()
+
+    # Crea un resumen
+    resumen = "El gráfico muestra la frecuencia de ventas de los productos:\n"
+    resumen += f"El producto más frecuente es '{producto_mas_frecuente}' con una frecuencia de {frecuencia_maxima} compras."
+
+    if mostrar_resumen:
+        print(resumen)
+
+    return resumen
+
+# Función para analizar la distribución de la cantidad vendida de productos
+def distribucion_de_la_cantidad_vendida_de_productos(df, mostrar_grafico, mostrar_resumen):
+    # Calcula la moda (valor más frecuente) de la cantidad vendida
+    moda = df['Quantity Ordered'].mode().values[0]
+
+    if mostrar_grafico:
+        # Muestra un histograma de la cantidad vendida
+        plt.hist(df['Quantity Ordered'], bins=20, edgecolor='k')
+        plt.xlabel('Cantidad Vendida')
+        plt.ylabel('Frecuencia')
+        plt.title('Distribución de Cantidad Vendida')
+        plt.show()
+
+    # Crea un resumen
+    resumen = "El gráfico muestra la distribución de la cantidad de productos vendidos en función de la frecuencia de ventas.\n"
+    resumen += f"La cantidad más frecuente vendida es {moda} unidades."
+
+    if mostrar_resumen:
+        print(resumen)
+
+    return resumen
+
+# Función para analizar las ventas por mes y día de la semana
+def ventas_por_mes_y_dia_de_la_semana(df, mostrar_grafico, mostrar_resumen):
+    
+
+    # Crea una tabla de calor (heatmap) que muestra la cantidad vendida en función del mes y el día de la semana
+    heatmap_data = df.pivot_table(index='Month', columns='Day of Week', values='Quantity Ordered', aggfunc='sum')
+    
+    # Encuentra el mes con las mayores ventas y el día de la semana con las mayores ventas
+    mes_max_ventas = heatmap_data.sum(axis=1).idxmax()
+    dia_max_ventas = heatmap_data.sum().idxmax()
+    
+    # Obtiene el valor máximo de ventas
+    max_ventas = heatmap_data.loc[mes_max_ventas, dia_max_ventas]
+    
+    # Encuentra el mes con las menores ventas y el día de la semana con las menores ventas
+    mes_min_ventas = heatmap_data.sum(axis=1).idxmin()
+    dia_min_ventas = heatmap_data.sum().idxmin()
+    
+    # Obtiene el valor mínimo de ventas
+    min_ventas = heatmap_data.loc[mes_min_ventas, dia_min_ventas]
+    
+    # Calcula el total de ventas en el conjunto de datos
+    total_ventas = heatmap_data.sum().sum()
+    
+    # Calcula el promedio de ventas por día de la semana
+    promedio_ventas_diario = heatmap_data.mean().mean()
+    
+    # Crea un resumen de los hallazgos
+    resumen = "En el análisis del mapa de calor de ventas por mes y día de la semana, se encontraron las siguientes estadísticas:\n"
+    resumen += f"El mes con la mayor cantidad de ventas es {mes_max_ventas}, con un total de {max_ventas} ventas.\n"
+    resumen += f"El día de la semana con la mayor cantidad de ventas es {dia_max_ventas}.\n"
+    resumen += f"El día de la semana con la menor cantidad de ventas es {dia_min_ventas}.\n"
+    resumen += f"El total de ventas en el conjunto de datos es de {total_ventas} unidades.\n"
+    resumen += f"El promedio de ventas por día de la semana es de aproximadamente {promedio_ventas_diario:.2f} unidades."
+
+    if mostrar_grafico:
+        # Muestra un mapa de calor de las ventas
+        plt.figure(figsize=(12, 6))
+        sns.heatmap(heatmap_data, cmap='YlGnBu', linewidths=.5)
+        plt.title('Mapa de Calor de Ventas por Mes y Día de la Semana')
+        plt.xlabel('Día de la Semana')
+        plt.ylabel('Mes')
+        plt.xticks(ticks=[0, 1, 2, 3, 4, 5, 6], labels=['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'])
+        plt.yticks(rotation=0)
+        plt.show()
+    
+    if mostrar_resumen:
+        print(resumen)
+    
+    return resumen
 
 
+def ventas_por_hora_del_dia_y_de_la_semana(df, mostrar_grafico, mostrar_resumen):
+    # Crea una tabla de calor (heatmap) que muestra la cantidad vendida en función de la hora del día y el día de la semana
+    heatmap_data = df.pivot_table(index='Hour', columns='Day of Week', values='Quantity Ordered', aggfunc='sum')
+    
+    # Define los nombres de los días de la semana
+    dias_semana = [ "Domingo","Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
+    
+    # Encuentra la hora de pico total y el día de la semana de pico total
+    hora_pico_total = heatmap_data.idxmax().values
+    dia_pico_total = heatmap_data.idxmax(axis=1).idxmax() 
+      # Encuentra el día de la semana pico total
+    dia_semana_pico_total = heatmap_data.loc[hora_pico_total, :].idxmax()
+    print("dia_pico_total")
+    print(dia_pico_total)
+    # Crea un resumen de los hallazgos
+    resumen = "Resumen del Mapa de Calor de Cantidad Vendida por Hora del Día y Día de la Semana:\n"
+    resumen += "El mapa de calor muestra las horas del día y los días de la semana con las mayores ventas.\n"
+  
 
-def dispersion_cantidad_vendida_por_producto(df,mostrar_grafico,mostrar_resumen):
-  resumen = "Resumen del Gráfico de Dispersión de Cantidad Vendida por Producto:"
-  resumen += "Este gráfico muestra la relación entre la cantidad vendida y los productos."
+    if mostrar_grafico:
+        # Muestra un mapa de calor de las ventas
+        plt.figure(figsize=(12, 6))
+        sns.heatmap(heatmap_data, cmap='YlGnBu', linewidths=.5)
+        plt.title('Mapa de Calor de Cantidad Vendida por Hora del Día y Día de la Semana')
+        plt.xlabel('Día de la Semana')
+        plt.ylabel('Hora del Día')  
+        plt.grid(True)
+        plt.show()
+    
+    if mostrar_resumen:
+        print(resumen)
+    
+    return resumen
 
-  if mostrar_grafico:
-    sns.scatterplot(x='Product', y='Quantity Ordered', data=df)
-    plt.xticks(rotation=90)
-    plt.xlabel('Producto')
-    plt.ylabel('Cantidad Vendida')
-    plt.title('Gráfico de Dispersión de Cantidad Vendida por Producto')      
-    plt.grid(True)
-    plt.show()
-  if mostrar_resumen:
-     print(resumen)
-  return resumen   
+
+# Función para crear un gráfico de dispersión de la cantidad vendida por producto
+def dispersion_cantidad_vendida_por_producto(df, mostrar_gráfico, mostrar_resumen):
+    # Define el resumen explicativo del gráfico de dispersión
+    resumen = "Resumen del Gráfico de Dispersión de Cantidad Vendida por Producto:\n"
+    resumen += "Este gráfico muestra la relación entre la cantidad vendida y los productos."
+
+    if mostrar_gráfico:
+        # Crea un gráfico de dispersión con los productos en el eje X y la cantidad vendida en el eje Y
+        sns.scatterplot(x='Product', y='Quantity Ordered', data=df)
+        plt.xticks(rotation=90)
+        plt.xlabel('Producto')
+        plt.ylabel('Cantidad Vendida')
+        plt.title('Gráfico de Dispersión de Cantidad Vendida por Producto')
+        plt.grid(True)
+        plt.show()
+
+    if mostrar_resumen:
+        # Muestra el resumen si se solicita
+        print(resumen)
+
+    return resumen
 
 def historial_de_ventas_por_semana(df,mostrar_grafico,mostrar_resumen):
   df['Order Date'] = pd.to_datetime(df['Order Date'])
