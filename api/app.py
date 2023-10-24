ef3d0ba13800dae37cf7abe4046ea82f5cb586b0
ocamarli <108758911+ocamarli@users.noreply.github.com>
2023-10-24 15:05:39 -0600

    actualizacion

--- api/app.py
+++ api/app.py
@@ -1,71 +1,62 @@
-import datetime
+from datetime import datetime, timedelta
 import json
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
-from requests import HTTPError
 from config import config
 from flask_cors import CORS
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tkinter as tk
+from tkinter import filedialog
 import pandas as pd
 import numpy as np
-from funciones import *
-import os
-from keras.models import load_model
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import load_model
 import seaborn as sns
 import matplotlib.pyplot as plt
+import json
 
 from funciones import *
 
 
-import csv
-if not os.path.exists('data.csv'):
-    # Si el archivo no existe, crea uno utilizando 
-    df = cargar_datos('https://raw.githubusercontent.com/jdanieln/UNIR_Investigacion_IA_Backend/main/data/data.csv')    
-
-    selected_columns = ["Product", "Quantity Ordered", "Order Date"]
-    df = seleccionar_columnas(df,selected_columns)
-    df = manejo_valores_faltantes(df)
-    df = tratamiento_columnas(df)    
-    try:
-    # Convierte el DataFrame en un archivo CSV
-        df.to_csv('data.csv', index=False)
-        print(f"Archivo CSV  creado en la misma carpeta del proyecto.")
-    except Exception as e:
-        print(f"Error al crear el archivo CSV: {e}")
-
-try:
-    # Intenta leer el archivo CSV como un DataFrame
-    df = pd.read_csv('data.csv')
-    print("El archivo CSV ok")
-except FileNotFoundError:
-    # Si el archivo no se encuentra, realiza otra acción (por ejemplo, muestra un mensaje)
-
-    print("El archivo CSV no se encontró en la misma carpeta del proyecto.")
- 
-
 def create_app(env):
     app = Flask(__name__)
     CORS(app)
-    app.config['DEBUG'] = env.DEBUG
+    app.config['DEBUG'] = True
     app.config.from_object(env)
-    app.config['df'] = df
+
     return app
+
 
 env = config['development']
 app = create_app(env)
+def convert_date(date_str):
+    try:
+        return pd.to_datetime(date_str, format='%m/%d/%Y %H:%M')
+    except ValueError:
+        return pd.to_datetime(date_str, format='%m/%d/%y %H:%M')
+
+@app.route('/upload', methods=['POST'])
+def upload_csv():
+    global df
+    df = cargar_datos('https://raw.githubusercontent.com/jdanieln/UNIR_Investigacion_IA_Backend/main/data/data.csv')    
+    return 'CSV en la solicitud.', 200
 
 flag_csv=False
 @app.route("/dataframe")
 def dataFrame():
+    df = cargar_datos('https://raw.githubusercontent.com/jdanieln/UNIR_Investigacion_IA_Backend/main/data/data.csv')    
+    selected_columns = ["Product", "Quantity Ordered", "Order Date"]
+    df = seleccionar_columnas(df,selected_columns)
+    df = manejo_valores_faltantes(df)
+    dfToFront=df.copy()
+    df = tratamiento_columnas(df)
 
-    df = app.config['df'] 
-    print(df.head(10))
-    df['Order Date'] = df['Order Date'].apply(convert_date_str)
     # Convertir el DataFrame a JSON
-    df_json = df.to_json(orient='records')
+    df_json = dfToFront.to_json(orient='records')
     resumen_top_10_productos_mas_vendidos=top_10_productos_mas_vendidos(df,False,True)
     resumen_frecuencia_de_ventas_por_producto=frecuencia_de_ventas_por_producto(df,False,True)
     resumen_distribucion_de_la_cantidad_vendida_de_productos=distribucion_de_la_cantidad_vendida_de_productos(df,False,True)
@@ -84,38 +75,40 @@ def dataFrame():
             'resumen_historial_de_ventas_por_semana': resumen_historial_de_ventas_por_semana             
         }
     }
+
     resultados_json = json.dumps(resultados_dict)
     return jsonify({"dataframe":df_json,"resumenes":resultados_json})
+
+@app.route("/")
 def root():
+
     return "Works!!"
-@app.route("/test")
-def test():
-    df = app.config['df']  # Obtener el DataFrame desde la configuración de la aplicación
-    # Imprimir los primeros 10 registros del DataFrame
-    first_10_records = df.head(10)
-    return str(first_10_records)
-@app.route("/createCSV")
-def createCSV(dataframe, csv_filename):
-    try:
-        # Convierte el DataFrame en un archivo CSV
-        dataframe.to_csv(csv_filename, index=False)
-        print(f"Archivo CSV '{csv_filename}' creado en la misma carpeta del proyecto.")
-        return "OK", 200
-    except Exception as e:
-        print(f"Error al crear el archivo CSV: {e}")
-        return "Error al crear el archivo CSV", 500
-def root():
-    return "Works!!"
-    
+
+
 @app.route("/getPrediction/<string:start_date>/<string:end_date>/<string:product>", methods=["GET"])
 def get_sales_by_date(start_date, end_date,product):
     try:
-        # Cargar el modelo previamente entrenado
-        loaded_model = load_model("./../modelo-ia/modelo.h5")
+        product_label_mapping = ""
+
+        with open('./modelo-ia/product_label_mapping.json', 'r') as file:
+            print("**")
+            product_label_mapping = json.load(file)
+
+        print("***")
+        encoded_product = product_label_mapping.get(product, -1)
+        print("encoded_product", encoded_product)
+
+        # Cargar el modelo desde el archivo .h5
+        loaded_model = load_model("modelo-ia/modelo.h5")
+
+        # Cargar los pesos del modelo desde el archivo .h5
+        loaded_model.load_weights("modelo-ia/pesos_modelo.h5")
 
          # Preprocesar las fechas y otros datos según sea necesario
         start_date = datetime.strptime(start_date, "%Y-%m-%d %H:%M:%S")
         end_date = datetime.strptime(end_date, "%Y-%m-%d %H:%M:%S")
+
+        total_product_predicted = 0
 
         # Inicializar una lista para almacenar las predicciones
         predictions_list = []
@@ -124,63 +117,32 @@ def get_sales_by_date(start_date, end_date,product):
         current_date = start_date
         while current_date <= end_date:
             # Crea un ejemplo de input_data para la fecha actual
-            input_data = np.array([[
-                current_date.year, current_date.month, current_date.day, current_date.hour, current_date.minute, current_date.weekday(),
-                product  # Añade la variable "product" codificada aquí si es necesario
-            ]])
+            input_data = np.array([[[
+                int(current_date.year), int(current_date.month), int(current_date.day), int(current_date.hour), int(current_date.minute), int(current_date.weekday()),
+                encoded_product  # Añade la variable "product" codificada aquí si es necesario
+            ]]])
 
             # Realizar la predicción
+            print("input_data", input_data)
             prediction = loaded_model.predict(input_data)[0][0]  # Tomar el valor de la predicción
+            print("prediction:", prediction)
 
-            # Almacena la fecha y la predicción en una lista
-            predictions_list.append({
-                "date": current_date.strftime("%Y-%m-%d %H:%M:%S"),
-                "prediction": prediction
-            })
+            total_product_predicted += float(prediction)
 
             # Incrementar la fecha en un intervalo (por ejemplo, 1 hora)
-            current_date += datetime.timedelta(hours=1)
+            current_date += timedelta(days=1)
 
         # Devolver las predicciones en formato JSON
         response = {
-            "predictions": predictions_list
+            "product": product,
+            "quantity_ordered": total_product_predicted
         }
-
+        # Responder JSON
         return jsonify(status=True, data=response), 200
 
     except Exception as e:
         return jsonify(status=False, error=str(e)), 500
 
-
-        print("startDate",start_date)
-        print("endDate",end_date)                    
-        # Aquí puedes realizar cualquier otro preprocesamiento necesario de los datos de entrada
-        # Por ejemplo, codificar la variable categórica "product" si es necesario
-        # Crear un ejemplo de input_data (asegúrate de que tenga las mismas características que se utilizaron durante el entrenamiento)
-        # En este ejemplo, se asume que tienes un conjunto de características que coincide con las que se utilizaron en el modelo
-        input_data = np.array([[
-        start_date.year, start_date.month, start_date.day, start_date.hour, start_date.minute, start_date.weekday(),
-        product  # Añade la variable "product" codificada aquí si es necesario
-        ]])
-
-        # Realizar la predicción
-        predictions = loaded_model.predict(input_data)
-
-        # Las predicciones se pueden ajustar según sea necesario antes de incluirlas en la respuesta JSON
-
-        # Convertir las predicciones a una lista de Python
-        predictions_list = predictions.tolist()
-
-        # Crear un diccionario que contenga las predicciones
-        response = {
-        "predictions": predictions_list
-        }
-        print(response)
-        return jsonify(status=True, data=response), 200
-
-    except Exception as e:
-        return jsonify(status=False, error=str(e)), 500
 
 if __name__ == '__main__':
-    app.run()
- 
+    app.run( host='0.0.0.0',port=5002)
